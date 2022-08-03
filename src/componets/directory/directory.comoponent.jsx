4b39d41332afc8a68a82eed97a658b35e03ece51
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-08-03 16:01:49 +0100

    router-dom

--- src/componets/directory/directory.comoponent.jsx
+++ src/componets/directory/directory.comoponent.jsx
@@ -1,9 +1,9 @@
 import CategoryItem from "../category-item/category-item.component";
 import './directory.style.scss'
-const Directory = ({ catergories }) => {
+const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
-      {catergories.map((category) => (
+      {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
     </div>
